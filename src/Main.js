cb92e2a77148c50363e132914626cc1f3e349037
Joshua Huston <joshua.david.huston@gmail.com>
2021-09-21 23:44:32 -0400

    Updated after code review

--- src/Main.js
+++ src/Main.js
@@ -5,8 +5,13 @@ class Main extends React.Component {
   render(){
     return(
       <>
-      <HornedBeasts title='manatee' img='test' description='rotund mythical sea creature'/>
-      <HornedBeasts title='unicorn' img='test' description='horned mythical creature'/>
+      <HornedBeasts 
+      title='manatee'
+      img='test' 
+      description='rotund mythical sea creature'/>
+      <HornedBeasts title='unicorn' 
+      img='test'
+      description='cross between rhino and horse'/>
       </>
     )
   }
