0867f514d98d1bbb71173ff50a87bf7b711d6b10
Joshua Huston <joshua.david.huston@gmail.com>
2021-09-29 22:24:02 -0400

    Fully functional Lab 3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Header from './Header.js';
 import Footer from './Footer.js';
 import Main from './Main.js';
+import SelectedBeast from './SelectedBeast';
 import dataJson from './data.json'
 
 
@@ -10,21 +11,44 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      totalBeasts: dataJson,
+      selectedBeast: {},
+      // hornBeastSelection: {},
       showModal: false,
-      data: dataJson
+      // hornSelection: false
     }
   }
 
+  // displayHornSelection = (horns) => {
+    //use filter on dataJson
+  //   const hornSelection = dataJson.find(beast => beast.horns === horns);
+  //   this.setState({ hornBeastSelection, hornSelection: true });
+  // }
+
+  displayAsModal = (name) => {
+    const selectedBeast = dataJson.find(beast => beast.title === name);
+    this.setState({ selectedBeast, showModal:true});
+  }
+
+  handleClose = () => this.setState({ showModal: false });
+ 
  
   render() {
     return (
     <>
     <Header />
     <Main 
-      toggleModal={this.toggleModal}
-      data={this.state.data}  />
-
+     
+      totalBeasts={this.state.totalBeasts}
+      displayAsModal={this.displayAsModal}
+      displayHornSelection={this.displayHornSelection}  />
+      
     <Footer />
+    <SelectedBeast 
+    selectedBeast={this.state.selectedBeast}
+    showModal={this.state.showModal}
+    handleClose={this.handleClose}
+    />
     </>
     
     );
