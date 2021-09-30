b25fda767c30b58fd95e75f02b279659b4b5da24
Joshua Huston <joshua.david.huston@gmail.com>
2021-09-29 23:54:34 -0400

    Added dropdown and filter by number of horns

--- src/App.js
+++ src/App.js
@@ -5,26 +5,21 @@ import Footer from './Footer.js';
 import Main from './Main.js';
 import SelectedBeast from './SelectedBeast';
 import dataJson from './data.json'
-
+import Form from 'react-bootstrap/Form';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      totalBeasts: dataJson,
+      hornBeastSelection: dataJson,
       selectedBeast: {},
-      // hornBeastSelection: {},
       showModal: false,
       // hornSelection: false
     }
   }
 
-  // displayHornSelection = (horns) => {
-    //use filter on dataJson
-  //   const hornSelection = dataJson.find(beast => beast.horns === horns);
-  //   this.setState({ hornBeastSelection, hornSelection: true });
-  // }
 
+ 
   displayAsModal = (name) => {
     const selectedBeast = dataJson.find(beast => beast.title === name);
     this.setState({ selectedBeast, showModal:true});
@@ -32,17 +27,43 @@ class App extends React.Component {
 
   handleClose = () => this.setState({ showModal: false });
  
- 
+  handleChange = (event) => {
+      let changedBeastArr = dataJson.filter((n) => {
+      if(event.target.value === 'all'){
+        return n.horns;
+    } else {
+      return (Number(event.target.value) === n.horns);
+    }
+    
+  });
+    this.setState({hornBeastSelection : changedBeastArr}) 
+
+  }
+
+
   render() {
     return (
     <>
+    
     <Header />
+    <Form id="myForm">
+          <Form.Group controlId="hornFrom.ControlSelect">
+            <Form.Control as="select" name="horns" onChange={this.handleChange}>
+              <option value="all">Select Number of Horns</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="100">100</option>
+            </Form.Control>
+          </Form.Group>
+          </Form>
     <Main 
      
-      totalBeasts={this.state.totalBeasts}
+      hornBeastSelection={this.state.hornBeastSelection}
       displayAsModal={this.displayAsModal}
-      displayHornSelection={this.displayHornSelection}  />
+      />
       
+    
     <Footer />
     <SelectedBeast 
     selectedBeast={this.state.selectedBeast}
